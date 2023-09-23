4fb67e5ae5c0309affd910d0dc6b176066f0b9c9
Eclarkhalid <eclarkhalid@gmail.com>
2023-09-23 16:59:52 +0300

    updating signup

--- server/utils/SecretToken.js
+++ server/utils/SecretToken.js
@@ -3,6 +3,6 @@ const jwt = require("jsonwebtoken");
 
 module.exports.createSecretToken = (id) => {
   return jwt.sign({ id }, process.env.TOKEN_KEY, {
-    expiresIn: 3 * 24 * 60 * 60,
+    expiresIn: 24 * 60 * 60, // Token will expire after 24 hours
   });
 };
